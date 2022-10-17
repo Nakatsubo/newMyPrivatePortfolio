786eeb2a1dcec2d07f9ad4134181c9fa23a51de8
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-10-17 22:05:21 +0900

    Netlifyのデプロイテスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
 
+import { defineNuxtConfig } from 'nuxt3'
+const { BASE_URL, API_KEY } = process.env;
+
+export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    baseURL: BASE_URL,
+    apiKey: API_KEY,
+  },
 })
