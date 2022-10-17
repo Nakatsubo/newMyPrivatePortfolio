5214134db4a8c9e1300426a154f01682308c4119
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-10-17 22:10:05 +0900

    Revert "Netlifyのデプロイテスト"
    This reverts commit 786eeb2a1dcec2d07f9ad4134181c9fa23a51de8.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-
-import { defineNuxtConfig } from 'nuxt3'
-const { BASE_URL, API_KEY } = process.env;
-
 export default defineNuxtConfig({
-  publicRuntimeConfig: {
-    baseURL: BASE_URL,
-    apiKey: API_KEY,
-  },
+
 })
